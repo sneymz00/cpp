171329f67648a3ce3ec69e68d1501ac75347c9b9
Sneyder Murillo <sneydermaza123@gmail.com>
2025-10-24 14:11:57 +0200

    funtional

--- utils.cpp
+++ utils.cpp
@@ -1,62 +1,76 @@
+#include "Span.hpp"
 
-
-
-void	convertFloat(const std::string& str)
+Span::Span() : _N(0)
 {
-	float	f = std::atof(str.c_str());
-	bool	tolerance = std::fabs(f - static_cast<int>(f)) < 0.0000000000001;
-
-	std::cout << "char: ";
-	if (f < 0 || f > 127)
-		std::cout << "impossible" << std::endl;
-	else
-	{
-		if (isprint(f))
-			std::cout << "'" << static_cast<char>(f) << "'" << std::endl;
-		else 
-			std::cout << "Non displayable" << std::endl;
-	}
-	std::cout << "int: ";
-	if (static_cast<long>(f) < MIN_INT || static_cast<long>(f) > MAX_INT)
-		std::cout << "impossible" << std::endl;
-	else
-		std::cout << static_cast<int>(f) << std::endl;
-	std::cout << "float: ";
-	if (f < MIN_FLOAT || f > MAX_FLOAT)
-		std::cout << "impossible" << std::endl;
-	else
-		std::cout << f << (tolerance ? ".0f" : "f") << std::endl;
-	std::cout << "double: " << static_cast<double>(f) << (tolerance ? ".0" : "") << std::endl;
 }
 
-void	convertDouble(const std::string& str)
+Span::Span(unsigned int N) : _N(N)
 {
-	double		d = std::atof(str.c_str());
-	bool		tolerance = std::fabs(d - static_cast<int>(d)) < 0.0000000000001;
+}
 
-	std::cout << "char: ";
-	if (d < 0 || d > 127)
-		std::cout << "impossible" << std::endl;
-	else
+Span::Span(const Span &other)
+{
+	*this = other;
+}
+
+Span &Span::operator=(const Span &other)
+{
+	if (this != &other)
 	{
-		if (isprint(d))
-			std::cout << "'" << static_cast<char>(d) << "'" << std::endl;
-		else 
-			std::cout << "Non displayable" << std::endl;
+		_N = other._N;
+		_v = other._v;
 	}
-	std::cout << "int: ";
-	if (d < MIN_INT || d > MAX_INT)
-		std::cout << "impossible" << std::endl;
-	else
-		std::cout << static_cast<int>(d) << std::endl;
-	std::cout << "float: ";
-	if (d < MIN_FLOAT || d > MAX_FLOAT)
-		std::cout << "impossible" << std::endl;
-	else
-		std::cout << static_cast<float>(d) << (tolerance ? ".0f" : "f") << std::endl;
-	std::cout << "double: ";
-	if (d < MIN_DOUBLE || d > MAX_DOUBLE)
-		std::cout << "impossible" << std::endl;
-	else
-		std::cout << d << std::endl;
+	return (*this);
+}
+
+Span::~Span()
+{
+}
+
+void	Span::addNumber(int n)
+{
+	if (_v.size() >= _N)
+		throw FullContainerException();
+	_v.push_back(n);
+}
+
+void	Span::addNumber(std::vector<int>::iterator begin, std::vector<int>::iterator end)
+{
+	if (_v.size() + std::distance(begin, end) > _N)
+		throw FullContainerException();
+	_v.insert(_v.end(), begin, end);
+}
+
+int		Span::shortestSpan()
+{
+	if (_v.size() <= 1)
+		throw NotEnoughNumbersException();
+	std::vector<int> tmp = _v;
+	std::sort(tmp.begin(), tmp.end());
+	int min = tmp[1] - tmp[0];
+	for (unsigned int i = 1; i < tmp.size(); i++)
+	{
+		if (tmp[i] - tmp[i - 1] < min)
+			min = tmp[i] - tmp[i - 1];
+	}
+	return (min);
+}
+
+int		Span::longestSpan()
+{
+	if (_v.size() <= 1)
+		throw NotEnoughNumbersException();
+	std::vector<int> tmp = _v;
+	std::sort(tmp.begin(), tmp.end());
+	return (tmp[tmp.size() - 1] - tmp[0]);
+}
+
+const char *Span::FullContainerException::what() const throw()
+{
+	return ("Container is full");
+}
+
+const char *Span::NotEnoughNumbersException::what() const throw()
+{
+	return ("Not enough numbers in container");
 }
